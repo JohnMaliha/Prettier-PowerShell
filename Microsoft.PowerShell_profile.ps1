b992c59109bd80a1772838176e0920b6e12baaf0
TheRealJohnM <32211116+JohnMaliha@users.noreply.github.com>
2024-06-25 21:08:14 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,8 +8,6 @@ $env:IsInPwsh = 1
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-#$env:COLORED_BUILD_ID = "`e[1;37m$env:BUILD_ID`e[0m"
-
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
 $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
